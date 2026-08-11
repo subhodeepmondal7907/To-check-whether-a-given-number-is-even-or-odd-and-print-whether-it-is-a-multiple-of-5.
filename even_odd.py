cecade2c832f9cca623982113ea0d3d765c82d4d
subhodeepmondal7907 <subhodeepmondal7907@gmail.com>
2026-08-11 23:51:29 +0530

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,9 @@
+n = int(input("Enter a number: "))
+if n % 2 == 0:
+    print(n," is an Even number.")
+else:
+    print(n," is an Odd number.")
+if n % 5 == 0:
+    print(n," is a multiple of 5.")
+else:
+    print(n," is not a multiple of 5.")
